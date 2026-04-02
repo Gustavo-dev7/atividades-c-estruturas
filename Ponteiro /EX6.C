5e8c5f23a7370c7fb96cac9992559b32e5b89711
Gustavo-dev7 <gustavopreiraa.lira@gmail.com>
2026-04-02 20:42:05 -0300

    EX6

---
+++ Ponteiro /EX6.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int v[5] = {1, 2, 3, 4, 5};
+    int *p = v;
+    for(int i = 0; i < 5; i++) {
+        printf("Elemento %d: %d\n", i, *(p + i));
+    }
+    return 0;
+}
