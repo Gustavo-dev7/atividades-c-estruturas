739a0a1538c1355c4ea20fa7ca1347a0864022be
Gustavo-dev7 <gustavopreiraa.lira@gmail.com>
2026-04-02 20:54:21 -0300

    Add program to generate random square matrix

---
+++ ALOCAÇÃO DINAMICA/x3_matriz_aleatoria.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int n, i, j;
+    int **matriz;
+    
+    printf("Digite a ordem da matriz quadrada: ");
+    scanf("%d", &n);
+    
+    matriz = (int **) malloc(n * sizeof(int *));
+    for (i = 0; i < n; i++) {
+        matriz[i] = (int *) malloc(n * sizeof(int));
+    }
+    
+    srand(time(NULL));
+    
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            matriz[i][j] = rand() % 100 + 1;
+        }
+    }
+    
+    printf("\nMatriz %dx%d com números aleatórios:\n", n, n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%3d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for (i = 0; i < n; i++) free(matriz[i]);
+    free(matriz);
+    
+    return 0;
+}
