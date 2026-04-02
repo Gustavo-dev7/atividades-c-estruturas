014c6cdc3bb464e2f5ff17e65ebeffeb8398c5bf
Gustavo-dev7 <gustavopreiraa.lira@gmail.com>
2026-04-02 20:39:28 -0300

    EX.1

---
+++ Ponteiro /ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int *p;
+    
+    p = &x;
+    
+    printf("Valor de x: %d\n", x);
+    printf("Endereço de x: %p\n", (void*)&x);
+    printf("Valor armazenado em p: %p\n", (void*)p);
+    printf("Valor apontado por p: %d\n", *p);
+    
+    return 0;
+}
